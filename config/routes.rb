e37432afff051005c0bd66063f16caf2c77b2179
Alan Carden <acarden120@gmail.com>
2015-03-13 12:06:59 +0000

    advertisement actions limited to show and index actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Bloccit::Application.routes.draw do
-  resources :advertisements
+  resources :advertisements, except: [:new, :create, :edit, :update, :destroy]
 
   get 'about' => 'welcome#about'
 
