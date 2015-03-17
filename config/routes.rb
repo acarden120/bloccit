72cd4100dea0f48e9bc3d810c7bdb4cf0e764133
Alan Carden <acarden120@gmail.com>
2015-03-17 14:27:51 +0000

    Associate and nested topics and posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Bloccit::Application.routes.draw do
+  get "topics/index"
+  get "topics/new"
+  get "topics/show"
+  get "topics/edit"
   devise_for :users
-  resources :posts #, except: [:show]
+
+   resources :topics do
+     resources :posts, except: [:index]
+   end
 
   get 'about' => 'welcome#about'
 
