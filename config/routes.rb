5a8fdf8f80d149898cae160bac3afb86430da1cc
Alan Carden <acarden120@gmail.com>
2015-03-17 19:01:40 +0000

    topic-post assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Bloccit::Application.routes.draw do
+  get "summaries/show"
   get "topics/index"
   get "topics/new"
   get "topics/show"
   get "topics/edit"
+
+#  get 'summary', to: :create
+#  get 'summary', to: :show
+
   devise_for :users
 
    resources :topics do
      resources :posts, except: [:index]
+   end
+
+   resources :posts do
+     resources :summaries, except: [:index, :edit]
    end
 
   get 'about' => 'welcome#about'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316192538) do
+ActiveRecord::Schema.define(version: 20150317173002) do
 
   create_table "advertisements", force: true do |t|
     t.string   "title"
@@ -37,10 +37,18 @@ ActiveRecord::Schema.define(version: 20150316192538) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.integer  "summary_id"
   end
 
+  add_index "posts", ["summary_id"], name: "index_posts_on_summary_id"
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "summaries", force: true do |t|
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "topics", force: true do |t|
     t.string   "name"
