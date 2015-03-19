84bc4c4aaa13163fbbb7bbf8269da76b16e4c1f8
Alan Carden <acarden120@gmail.com>
2015-03-19 22:57:11 +0000

    uploading images checkpoint

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316192538) do
+ActiveRecord::Schema.define(version: 20150319202427) do
 
   create_table "advertisements", force: true do |t|
     t.string   "title"
@@ -37,10 +37,18 @@ ActiveRecord::Schema.define(version: 20150316192538) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.integer  "summary_id"
   end
 
+  add_index "posts", ["summary_id"], name: "index_posts_on_summary_id"
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "summaries", force: true do |t|
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "topics", force: true do |t|
     t.string   "name"
@@ -69,6 +77,7 @@ ActiveRecord::Schema.define(version: 20150316192538) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
