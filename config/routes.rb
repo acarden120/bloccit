783b2d6c71d0ee6eccc7fe43aaf46b4579043fab
Alan Carden <acarden120@gmail.com>
2015-03-26 19:18:57 +0000

    updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Bloccit::Application.routes.draw do
-  get "summaries/show"
-  get "topics/index"
-  get "topics/new"
-  get "topics/show"
-  get "topics/edit"
-
-#  get 'summary', to: :create
-#  get 'summary', to: :show
 
   devise_for :users
 
