5946fa6c2891a084170eecd5f1777fa57e880300
Alan Carden <acarden120@gmail.com>
2015-03-30 22:30:32 +0100

    Updated rspec assignment

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,14 +3,13 @@ describe Vote do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
         # this assumes that the method returns a boolean indicating success or failure
-        down_vote = Vote(-1)
-        expect(down_vote).to eq(true)
+        up_vote1 = Vote.new(value: 1)
+        down_vote = Vote.new(value: -1)
+        up_vote2 = Vote.new(value: 5)
 
-        up_vote1 = Vote(1)
-        expect(up_vote1).to eq(true)
-
-        up_vote2 = Vote(4)
-        expect(up_vote2.to eq(false)
+        expect(up_vote1.valid?).to eq (true)
+        expect(down_vote.valid?).to eq(true)
+        expect(up_vote2.valid?).to eq(false)
       end
     end
   end
