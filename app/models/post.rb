2c5decd5d24a1f5894934dff932f82bf6d5851bc
Alan Carden <acarden120@gmail.com>
2015-04-03 21:29:08 +0100

    Complete private topics assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,6 @@ class Post < ActiveRecord::Base
 
   default_scope { order('rank DESC') }
 
-#  mount_uploader :avatar, AvatarUploader
-
   def up_votes
     votes.where(value: 1).count
   end
@@ -22,15 +20,13 @@ class Post < ActiveRecord::Base
     votes.sum(:value)
   end
 
-   def update_rank
-     age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
-     new_rank = points + age_in_days
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+    new_rank = points + age_in_days
  
-     update_attribute(:rank, new_rank)
-   end
+    update_attribute(:rank, new_rank)
+  end
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-#  validates :topic, presence: true
-#  validates :user, presence: true
 end
