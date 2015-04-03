5f7d9e708aaff9d4d850f3a8a4b0701e773dca0e
Alan Carden <acarden120@gmail.com>
2015-04-03 22:00:58 +0100

    update to vote.rb

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,6 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  belongs_to :vote
 
   validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }
 
