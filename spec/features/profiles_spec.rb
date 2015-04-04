5d9b92a3cf6cb336e6db665fc67be109ea117fca
Alan Carden <acarden120@gmail.com>
2015-04-04 20:17:53 +0100

    Complete public profiles assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,4 +1,7 @@
- require 'rails_helper'
+  require 'rails_helper'
+
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   # include TestFactories and so the method is being added directly to the spec
   def authenticated_user(options={})
@@ -20,17 +23,22 @@
     Post.create(post_options)
   end
 
- describe "Visiting profiles" do
+  def comment_without_email
+    @comment = @post.comments.build(user: @user, body: "Test comment")
+    allow(@comment).to receive(:send_favorite_emails)
+    @comment.save
+    @comment
+  end
+
+  describe "Visiting profiles" do
 #	include TestFactories
 
     before do
       @user = authenticated_user
       @post = associated_post(user: @user)
-	  @comment = Comment.new(user: @user, body: "A Comment")
-      allow(@comment).to receive(:send_favorite_emails)
-      @comment.save
+      comment_without_email
     end
- 
+
     describe "not signed in" do
       it "shows profile" do
         visit user_path(@user)
@@ -40,4 +48,19 @@
         expect( page ).to have_content(@comment.body)
       end
     end
+
+    describe "signed in" do
+      before do
+        login_as(@user, :scope => :user)
+      end
+
+      it "shows profile" do
+        visit user_path(@user)
+        expect(current_path).to eq(user_path(@user))
+        expect( page ).to have_content(@user.name)
+        expect( page ).to have_content(@post.title)
+        expect( page ).to have_content(@comment.body)
+      end
+    end
+
  end
