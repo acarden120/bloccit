e80377a64377163ce31e6e9bc39a728b2818e69b
Alan Carden <acarden120@gmail.com>
2015-04-09 19:57:42 +0100

    updated user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,49 +2,29 @@ require 'rails_helper'
 
 describe User do
 
+# include TestFactories
+
   describe "#favorited(post)" do
 
     before do
+      @valid_post = create(:post, user: @user)
+      @second_valid_post = create(:post, user: @user)     
+      @non_favorited = create(:post, user: @user)
       @user = create(:user)
-      @post = create(:post, user: @user)
-    end
-
-    it "returns `nil` if the user has not favorited the post" do
-      expect( @user.favorited(@post) ).to eq(nil)
     end
 
     it "returns the appropriate favorite if it exists" do
-      @favorite = Favorite.new(user_id: @user.id, post_id: @post.id)
-      @favorite.save
-      expect( @user.favorited(@post) ).to eq( @favorite )
+      @favorite = Favorite.create(post: @valid_post, user: @user)
+      expect( @user.favorited(@valid_post) ).to eq(@favorite)
     end
 
-  end
+    it "returns `nil` if the user has not favorited the post" do
+      expect(@user.favorited(@non_favorited)).to eq(nil)
+    end
 
-  describe ".top_rated" do
- 
-    before do
-      @user1 = create(:user)
-      post = create(:post, user: @user1)
-      create(:comment, user: @user1, post: post)
- 
-      @user2 = create(:user)
-      post = create(:post, user: @user2)
-      2.times { create(:comment, user: @user2, post: post) }
-    end
- 
-    it "returns users ordered by comments + posts" do
-      expect( User.top_rated ).to eq([@user2, @user1])
-    end
- 
-    it "stores a `posts_count` on user" do
-      users = User.top_rated
-      expect( users.first.posts_count ).to eq(1)
-    end
- 
-    it "stores a `comments_count` on user" do
-      users = User.top_rated
-      expect( users.first.comments_count ).to eq(2)
+    it "returns `nil` if the user has favorited another post" do
+      @favorite = Favorite.create(post: @valid_post, user: @user)
+      expect(@user.favorited(@non_favorited)).to eq(nil)
     end
   end
 end
